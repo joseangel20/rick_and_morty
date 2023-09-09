acd313c3f1a97f4ab5da256d65e36e807a3a2b73
joseangel20 <joseangel20@hotmail.es>
2023-09-09 11:18:47 -0400

    Se agrego la caracteristica de async/await en las operaciones asincroicas

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -2,30 +2,30 @@ const axios = require("axios");
 
 const URL = "https://rickandmortyapi.com/api/character/";
 
-function getCharById(req, res) {
+async function getCharById(req, res) {
   const { id } = req.params;
-  
+
   if (isNaN(+id)) return res.status(404).json({ error: "Not fount" });
 
-  axios(URL + id)
-    .then(({ data }) => {
-      const { id, status, name, species, origin, image, gender } = data;
+  try {
+    const response = await axios(URL + id);
 
-      const character = {
-        id,
-        status,
-        name,
-        species,
-        origin:origin.name,
-        image,
-        gender,
-      };
+    const { status, name, species, origin, image, gender } = response.data;
 
-      res.status(200).json(character);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: error.message });
-    });
+    const character = {
+      id,
+      status,
+      name,
+      species,
+      origin: origin.name,
+      image,
+      gender,
+    };
+
+    res.status(200).json(character);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 module.exports = getCharById;
